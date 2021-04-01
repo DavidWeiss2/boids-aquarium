5599f480c1a7c8ea7fb8f02a8d42765c4426e29b
David Weiss <DavidWeiss2@gmail.com>
2021-04-01 08:20:01 +0300

    random flocks that avoid each others.

--- sketch.js
+++ sketch.js
@@ -1,12 +1,57 @@
-"use strict";
+// Flocking
+// Daniel Shiffman
+// https://thecodingtrain.com
+
+// https://thecodingtrain.com/CodingChallenges/124-flocking-boids.html
+// https://youtu.be/mhjuuHl6qHM
+// https://editor.p5js.org/codingtrain/sketches/ry4XZ8OkN
+
+let flocks = [];
+
+flocksUpdate = () => {
+  for (let boid of flocks) {
+    boid.edges();
+    boid.flock(flocks);
+    boid.update();
+    boid.show();
+  }
+};
+
+let alignSlider, cohesionSlider, separationSlider;
 
 function setup() {
-  // put setup code here
-  createCanvas(width, height, WEBGL);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+
+  alignSlider = createSlider(0, 2, 1, 0.1);
+  alignSlider.position(width/4,10);
+  cohesionSlider = createSlider(0, 2, 1, 0.1);
+  cohesionSlider.position(width/4*2,10);
+  separationSlider = createSlider(0, 2, 1.5, 0.1);
+  separationSlider.position(width/4*3,10);
+
+  numberOfFlocks = Math.ceil(random(1, 10));
+  for (let f = 0; f < numberOfFlocks; f++) {
+    let flock = [];
+    let colorvec = p5.Vector.random3D();
+    let colorr = color(colorvec.x * 255, colorvec.y * 255, colorvec.z * 255);
+    let numberOfBoids = random(50);
+    for (let i = 0; i < numberOfBoids; i++) {
+      flock.push(new Boid(colorr));
+    }
+    flocks = flocks.concat(flock);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+document.oncontextmenu = function() {
+  return false;
 }
 
 function draw() {
-  background(153);
-  line(0, 0, width, height);
-  // put drawing code here
+  background(51);
+  flocksUpdate();
 }
